9589fa978e4bf3324bc4f7383083f4e8ccac7465
Vendin <v.a.5@list.ru>
2015-09-21 17:44:31 +0300

    pull

--- app/src/main/java/com/example/av/androidtranslate/MainActivity.java
+++ app/src/main/java/com/example/av/androidtranslate/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.av.androidtranslate;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,26 +14,37 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.apache.http.client.utils.URIBuilder;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     HashMap<Integer, String> hashMapLanguage = new HashMap<>();
-    private final String key = "trnsl.1.1.20150920T100138Z.be418cc4b6842e02\n" +
-            ".b4f643f222a54b8ff78e7a738fe291d8a76f511a";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initHashMapLanguage();
+        NetworkThread networkThread = new NetworkThread(this);
+
+        networkThread.start();
 
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -46,38 +59,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(getApplicationContext(), fromAt, Toast.LENGTH_SHORT);
                 toast.show();
 
-//                Intent intent = new Intent(MainActivitya.this , Activity0.class);
-//                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this , TranslateActivity.class);
+                startActivity(intent);
 
             }
         });
 
     }
-
-
-
-
-    public void initHashMapLanguage(){
-        this.hashMapLanguage.put(1, "en");
-        this.hashMapLanguage.put(2, "ar");
-        this.hashMapLanguage.put(3, "nl");
-        this.hashMapLanguage.put(4, "el");
-        this.hashMapLanguage.put(5, "da");
-        this.hashMapLanguage.put(6, "he");
-        this.hashMapLanguage.put(7, "ga");
-        this.hashMapLanguage.put(8, "it");
-        this.hashMapLanguage.put(9, "zh");
-        this.hashMapLanguage.put(10, "ko");
-        this.hashMapLanguage.put(11, "la");
-        this.hashMapLanguage.put(12, "de");
-        this.hashMapLanguage.put(13, "no");
-        this.hashMapLanguage.put(14, "ru");
-        this.hashMapLanguage.put(15, "fr");
-        this.hashMapLanguage.put(16, "ja");
-
-    }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -99,5 +87,76 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // TODO: Нормальная обработка исключений
+    public void dispatchAPIResponse(String apiResponse) {
+        try {
+            JSONObject result = new JSONObject(apiResponse);
+            int code = result.getInt("code");
+            if (HttpURLConnection.HTTP_OK == code) {
+                Object text = result.getJSONObject("langs");
+                Log.e("Response code:", String.valueOf(text.toString()));
+                Toast toast = Toast.makeText(getApplicationContext(), text.toString(), Toast.LENGTH_SHORT);
+                toast.show();
+                //MainActivity.this.runOnUiThread(new TranslateActivity.OutputSetter(text));
+            } else {
+                // TODO: failed
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+class NetworkThread extends Thread {
+    private MainActivity callback;
+    private final String key = "trnsl.1.1.20150920T100138Z.be418cc4b6842e02b4f643f222a54b8ff78e7a738fe291d8a76f511a";
+
+    NetworkThread(MainActivity act){
+        callback = act;
+    }
+
+    @Override
+    public void run() {
+        callback.dispatchAPIResponse(getAPIResponse());
+    }
+
+    @Nullable
+    private String getAPIResponse() {
+        StringBuilder result = new StringBuilder();
+        try {
+            URIBuilder builder = new URIBuilder("https://translate.yandex.net/api/v1.5/tr.json/getLangs");
+            builder.addParameter("key", key);
+            builder.addParameter("ui", "ru");
+
+            URL url = builder.build().toURL();
+            Log.v("formed url", url.toString());
+
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            Log.v("Response code:", String.valueOf(connection.getResponseCode()));
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            return result.toString();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
